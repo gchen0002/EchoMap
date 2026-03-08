c6032ab8caf6769ab4a18c634f01e230f5663cc7
Guanying Chen <xnohohsd1@gmail.com>
2026-03-07 19:11:16 -0800

    fix: fixed node.js middle ware not being supported

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware(
-  () => {},
-  {
-    debug: process.env.NODE_ENV === "development",
-  }
-);
+export default clerkMiddleware({
+  debug: process.env.NODE_ENV === "development",
+});
 
 export const config = {
   matcher: [
